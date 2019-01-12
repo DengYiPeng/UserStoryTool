ac59b21d98d2e0a021567f67b940ed8e2f2727ec
DengYiPeng <707969656@qq.com>
2019-01-12 21:25:15 +0800

    [Develop] 实现User的Controller层

---
+++ src/main/java/edu/nju/stories/form/LoginForm.java
@@ -0,0 +1,10 @@
+package edu.nju.stories.form;
+
+import lombok.Data;
+
+@Data
+public class LoginForm {
+
+    private String email;
+    private String password;
+}
--- src/main/java/edu/nju/stories/rest/UserRest.java
+++ src/main/java/edu/nju/stories/rest/UserRest.java
@@ -1,5 +1,6 @@
 package edu.nju.stories.rest;
 
+import edu.nju.stories.form.LoginForm;
 import edu.nju.stories.form.RegisterForm;
 import edu.nju.stories.service.UserService;
 import edu.nju.stories.vo.SimpleResponse;
@@ -22,5 +23,17 @@ public class UserRest {
     public SimpleResponse register(@RequestBody RegisterForm form){
         String token = userService.register(form.getEmail(), form.getRegisterCode(), form.getPassword());
         return SimpleResponse.OK(token);
+    }
+
+    @PostMapping(value = "/login")
+    public SimpleResponse login(@RequestBody LoginForm form){
+        String token = userService.login(form.getEmail(), form.getPassword());
+        return SimpleResponse.OK(token);
+    }
+
+    @PostMapping(value = "/logout")
+    public SimpleResponse logout(@RequestHeader("token") String token){
+        boolean result = userService.logout(token);
+        return SimpleResponse.OK(result);
     }
 }
