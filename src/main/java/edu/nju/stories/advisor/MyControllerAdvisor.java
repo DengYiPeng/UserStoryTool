c2e49ffc97874385102ce9761e93ba4275927909
DengYiPeng <707969656@qq.com>
2019-01-14 22:59:25 +0800

    [Develop] 添加统一异常处理逻辑

---
+++ src/main/java/edu/nju/stories/advisor/MyControllerAdvisor.java
@@ -0,0 +1,38 @@
+package edu.nju.stories.advisor;
+
+import edu.nju.stories.constants.ErrorCode;
+import edu.nju.stories.exception.LogicException;
+import edu.nju.stories.vo.SimpleResponse;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class MyControllerAdvisor {
+
+    /**
+     * 全局异常捕捉处理
+     * @param ex
+     * @return
+     */
+    @ResponseBody
+    @ExceptionHandler(value = Exception.class)
+    public SimpleResponse errorHandler(Exception ex) {
+        if (ex instanceof LogicException){
+            LogicException exception = (LogicException) ex;
+            return new SimpleResponse(exception.getErrCode(), ex.getMessage());
+        }else{
+            return new SimpleResponse(ErrorCode.UNKNOWN, ex.getMessage());
+        }
+    }
+
+    /**
+     * 拦截捕捉自定义异常 MyException.class
+     * @param ex
+     * @return
+     */
+    @ResponseBody
+    @ExceptionHandler(value = LogicException.class)
+    public SimpleResponse myErrorHandler(LogicException ex) {
+        return new SimpleResponse(ex.getErrCode(), ex.getMessage());
+    }
+
+}
--- src/main/java/edu/nju/stories/constants/ErrorCode.java
+++ src/main/java/edu/nju/stories/constants/ErrorCode.java
@@ -2,6 +2,8 @@ package edu.nju.stories.constants;
 
 public class ErrorCode {
 
+    public static final int UNKNOWN = 0;
+
     public static final int NOT_LOGIN = 100;
 
     public static final int NO_PERMISSION = 200;
